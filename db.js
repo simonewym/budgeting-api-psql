b7ca2ec721eef7dc99df19f0254381cb4d136482
Simone Wong <simonewong@Simone-MBA-M2.fritz.box>
2024-05-14 22:36:35 +0200

    Fix PORT variable

--- db.js
+++ db.js
@@ -3,13 +3,14 @@ const DATABASE_USER = process.env.DATABASE_USER;
 const DATABASE_HOST = process.env.DATABASE_HOST;
 const DATABASE = process.env.DATABASE;
 const DATABASE_PW = process.env.DATABASE_PW;
+const PORT = process.env.PORT;
 
 const pool = new Pool({
     user: DATABASE_USER,
     host: DATABASE_HOST,
     database: DATABASE,
     password: DATABASE_PW,
-    port: 5432
+    port: PORT
 });
 
 module.exports = pool;
