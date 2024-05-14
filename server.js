30af438e65477c286157f1aafff798a148bb5e46
Simone Wong <simonewong@Simone-MBA-M2.fritz.box>
2024-05-14 22:41:10 +0200

    Fix server.js port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const envelopeRoutes = require('./src/envelopes/routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;;
 
 app.use(bodyParser.json())
 
